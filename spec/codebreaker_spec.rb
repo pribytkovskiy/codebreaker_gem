da51adea2c9a93601995806f469402505f517580
Dima <dr.trem86@gmail.com>
2019-01-06 14:05:14 +0200

    fix codebreaker_spec

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Codebreaker do
   it "has a version number" do
     expect(Codebreaker::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
